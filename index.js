fc6ac07190ceb40df42cc0b083d9b92616e5b6d0
Sushaan Maharjan <sushaanmaharjan1@gmail.com>
2021-09-15 08:39:43 +0545

    Node JS

---
+++ index.js
@@ -0,0 +1,25 @@
+let a=1;
+let b="Hello World";
+let c=true;
+let d=1.5;
+let m=a+d
+console.log("Addition",m)
+console.log(a)
+console.log(typeof a)
+
+const z="Hello world";
+console.log(z)
+
+let iims=null;
+console.log(iims)
+
+let time=new Date;
+console.log(time)
+
+let person=
+{
+    name:"Sushaan",
+    age:20,
+    address:"DALLU",
+}
+console.log("This person's name is ",person.name)
