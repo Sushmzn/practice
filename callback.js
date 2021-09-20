d203c899b33a3dc8a4dd9663e6818b5966dedd86
Sushaan Maharjan <sushaanmaharjan1@gmail.com>
2021-09-20 08:10:22 +0545

    conditional.js

--- callback.js
+++ callback.js
@@ -6,6 +6,10 @@ const makestrlowercase=(String)=>{
     return String.toLowerCase();
 };
 
+const padstring=(String)=>{
+    return string.padstart(10,"0")
+}
+
 const passuserinput=(input, callback) =>{
     input="Hello "+ input;
     return callback(input)
@@ -13,3 +17,17 @@ const passuserinput=(input, callback) =>{
 
 console.log(passuserinput("IIMS",makestrlowercase))
 console.log(passuserinput("iims",makestruppercase))
+
+//Single line arrow function
+const add =(a,b) => a+b;
+
+const sub =(a,b) => a-b;
+
+const mul =(a,b) => a*b;
+
+const callbackexample = (a,b,mathcallback) =>{
+    console.log(mathcallback(a,b));
+}
+callbackexample(2,2,add);
+callbackexample(2,2,sub);
+callbackexample(2,2,mul);
---
+++ conditional.js
@@ -0,0 +1,38 @@
+const age=25;
+if (age<=20){
+    console.log("You are in teen age.")
+}
+else if(age>20 && age<=29) 
+{
+    console.log("You are in your twenties.")
+}
+else
+{
+    console.log("You are old.")
+}
+
+let day='sunday';
+switch(day)
+{
+    case'monday':
+    console.log("It is monday")
+    break;
+    case'tuesday':
+    console.log("It is tuesday")
+    break;
+    case'wednesday':
+    console.log("It is wednesday")
+    break;
+    case'thursday':
+    console.log("It is thursday")
+    break;
+    default:
+    console.log("It is friday")
+
+}
+
+let c=0;
+while(c<=10){
+    console.log(c)
+    c++
+}
